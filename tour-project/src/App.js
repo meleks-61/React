c1e9cbfb6ff8fe13f3e9d4958d2e5391531dfb47
Melek <maibalonn@gmail.com>
2021-08-18 12:04:37 +0300

    projeckt1

---
+++ tour-project/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./components/header/Header";
+import Navbar from "./components/navbar/Navbar"
+import Card from "./components/cards/Card"
+import "./App.css"
+
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Header/>
+      <Card/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ tour-project/src/components/cards/Card.js
@@ -0,0 +1,59 @@
+import React ,{ useState } from "react";
+import { data } from "../../helpers/Data";
+
+import "./Card.css";
+
+function Card() {
+    const [showImage, setShowImage] = useState(true);
+
+  return (
+    <div className="card-container" onClick={()=> setShowImage(!showImage)}>
+        {showImage ? ( 
+            <>
+            {data.map((card ) => {
+        return (
+          <div className="cards" key={card.id}>
+            
+            <img src={card.image} alt={card.title} />
+            
+          </div>
+        );
+      })}
+      </>
+      ):(
+          <>
+        {data.map((card ) => {
+            return (
+              <div className="cards" key={card.id}>
+                <div className="title">
+                  <h2>{card.title}</h2>
+                </div>
+                <img src={card.image} alt={card.title} />
+                <div className="card-over">
+                  <p> {card.desc}</p>
+                </div>
+              </div>
+            );
+          })}
+          </>
+
+      )}
+     
+    </div>
+  );
+}
+export default Card;
+
+/* <div className="card-container">
+<div className="cards">
+  <div className="title">
+    <h2> {data[0].title}</h2>
+  </div>
+
+  <img src={data[0].image} alt={data[0].title} />
+  <div className="card-over">
+    <p> {data[0].desc}</p>
+  </div>
+</div>
+
+</div> */
